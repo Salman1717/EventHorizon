4637cbfd37a8205890383a8ae5a919f5c336db5b
Salman1717 <salmanmhaskar17@gmail.com>
2026-01-07 16:55:20 +0530

    Initial Commit

---
+++ EventHorizon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EventHorizon
+//
+//  Created by Salman Mhaskar on 07/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EventHorizon/EventHorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHorizonApp.swift
+//  EventHorizon
+//
+//  Created by Salman Mhaskar on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct EventHorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
